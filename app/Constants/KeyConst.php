d049a056c129974f99e528aa3417950afe8e84ee
Daniele Barbaro <barbaro.daniele@gmail.com>
2022-02-11 10:23:15 +0100

    Create a static abstract class for keys

---
+++ app/Constants/KeyConst.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constants;
+
+abstract class KeyConst
+{
+    const KEY_1 = "TUWYC";
+    const KEY_2 = "DeNda";
+    const KEY_3 = "tkz0w";
+    const KEY_4 = "kVZLp";
+    const KEY_5 = "g0sKJ";
+}
